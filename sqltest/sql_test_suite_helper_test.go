c2e5a5c6c39d9f2e1a19c09cd66c733ca8ac52ca
Satoshi Konno <skonno@cybergarage.org>
2023-03-17 12:53:19 +0900

    Add TestEmbedSQLTestSuite()

---
+++ sqltest/sql_test_suite_helper_test.go
@@ -0,0 +1,36 @@
+// Copyright (C) 2020 The go-sqltest Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqltest
+
+import (
+	"testing"
+
+	"github.com/cybergarage/go-sqltest/sqltest/test"
+)
+
+func TestEmbedSQLTestSuite(t *testing.T) {
+	suite, err := NeweEmbedSQLTestSuite(test.EmbedTests)
+	if err != nil {
+		t.Error(err)
+	}
+	for _, tst := range suite.Tests {
+		t.Run(tst.Name(), func(t *testing.T) {
+			scn := tst.Scenario
+			if scn == nil || (len(scn.Queries) == 0) {
+				t.Errorf("%s scenario is empty", tst.Name())
+			}
+		})
+	}
+}
