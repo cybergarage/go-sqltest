b44c8a0bc81237218f4142800495d1b0cbd9de32
Satoshi Konno <skonno@cybergarage.org>
2023-09-03 01:46:18 +0900

    Update smpl_crud.pl

--- sqltest/test/smpl_crud.pl
+++ sqltest/test/smpl_crud.pl
@@ -3,140 +3,56 @@
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
+use strict;
+use warnings;
+
+my $pr_key_type = "";
+if (1 <= @ARGV){
+  $pr_key_type = $ARGV[0];
+}
+
 print<<HEADER;
-// Copyright (C) 2022 The go-sqltest Authors. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+- Copyright (C) 2020 The go-sqltest Authors. All rights reserved.
+-
+- Licensed under the Apache License, Version 2.0 (the "License");
+- you may not use this file except in compliance with the License.
+- You may obtain a copy of the License at
+-
+-  http:-www.apache.org/licenses/LICENSE-2.0
+-
+- Unless required by applicable law or agreed to in writing, software
+- distributed under the License is distributed on an "AS IS" BASIS,
+- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+- See the License for the specific language governing permissions and
+- limitations under the License.
 
-package system
-
-type DataType struct {
-	name string
-	oid  OID
-	size int
-}
-
-// DataType represents a PostgreSQL data type.
-var dataTypes = map[OID]*DataType{}
-
-// GetDataType returns a data type for the specified OID.
-func GetDataType(oid OID) (*DataType, error) {
-	dt, ok := dataTypes[oid]
-	if !ok {
-		return nil, newDataTypeNotFound(oid)
-	}
-	return dt, nil
-}
-
-func newDataType(name string, oid OID, size int) *DataType {
-	dt := &DataType{
-		name: name,
-		oid:  oid,
-		size: size,
-	}
-	return dt
-}
-
-// Name returns the data type name.
-func (dt *DataType) Name() string {
-	return dt.name
-}
-
-// OID returns the data type OID.
-func (dt *DataType) OID() OID {
-	return dt.oid
-}
-
-// Size returns the data type size.
-func (dt *DataType) Size() int {
-	return dt.size
-}
-
-// FormatCodeFrom returns a format code from the specified data type.
-func (dt *DataType) FormatCode() FormatCode {
-	switch dt.oid {
-	case Name:
-		return TextFormat
-	case Text:
-		return TextFormat
-	case Varchar:
-		return TextFormat
-	}
-	return BinaryFormat
-}
-
-func init() { //nolint: gochecknoinits, maintidx
 HEADER
 
-my $pg_type_file = "res/pg_type.csv";
+my $pg_type_file = "data/data_type.pict";
 open(IN, $pg_type_file) or die "Failed to open $pg_type_file: $!";
 my $line_no = 0;
-my $name_idx = -1;
-my $oid_idx = -1;
-my $typ_len_idx = -1;
+my $pr_key_idx = -1;
 while(<IN>){
   $line_no++;
   chomp($_);
-  my @row = split(/,/, $_, -1);
+  my @row = split(/\t/, $_, -1);
   if ($line_no <= 1) {
+    print "CREATE TABLE test (\n";  
     for (my $i = 0; $i < scalar(@row); $i++) {
-      if ($row[$i] eq "typname") {
-        $name_idx = $i;
+      my $type_name = $row[$i];
+      my $column_name = "col" . $type_name;
+      if ($type_name eq $pr_key_type) {
+        print "\t" . $column_name . " " . $type_name . " PRIMARY KEY\n";  
+      } else {
+        print "\t" . $column_name . " " . $type_name . "\n";  
       }
-      if ($row[$i] eq "oid") {
-        $oid_idx = $i;
-      }
-      if ($row[$i] eq "typlen") {
-        $typ_len_idx = $i;
-      }      
     }
-    if ($name_idx < 0) {
-      die "Failed to find typname column";
-    }
-    if ($oid_idx < 0) {
-      die "Failed to find oid column";
-    }
-    if ($typ_len_idx < 0) {
-      die "Failed to find typlen column";
-    }
-    next;
+    print ");\n";  
+    print "{\n";  
+    print "}\n";  
   }
-  my $name = $row[$name_idx];
-  my $oid = $row[$oid_idx];
-  my $size = $row[$typ_len_idx];
-
-  if ($name =~ /^_/){
-    next;
-  }
-  # check nil rows
-  if (length($name)<=0 || length($oid)<=0 || length($size)<=0) {
-    next;
-  }
-  # UpperCase
-  # $name =~ s/([a-z])/\u$1/g;
-  # CamelCase
-  $name =~ s/_([a-z])/\u$1/g;
-  $name =~ s/^([a-z])/\u$1/;
-  # stylecheck
-  my @patterns = ('Sql', 'Xml', 'Json', 'Uuid', 'Db', 'Ts', 'Acl');
-  my @replacements = ('SQL', 'XML', 'JSON', 'UUID', 'DB', 'TS', 'ACL');
-  for (my $i = 0; $i < scalar(@patterns); $i++) {
-    if ($name =~ /$patterns[$i]/) {
-      $name =~ s/$patterns[$i]/$replacements[$i]/g;
-    }
-  }
-  print "\tdataTypes[" . $oid . "] = newDataType(\"" . $name . "\", " . $oid . ", " . $size . ")\n";  }
-  close(IN);
+}
+close(IN);
 
 print<<FOTTER;
 }
