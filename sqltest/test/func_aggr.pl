90542017cbfcd14ac41c5b627268dd7eb6af6b07
Satoshi Konno <skonno@cybergarage.org>
2023-09-11 12:34:38 +0900

    Update func_aggr.pl

--- sqltest/test/func_aggr.pl
+++ sqltest/test/func_aggr.pl
@@ -6,6 +6,11 @@
 use strict;
 use warnings;
 
+my $data_type = "";
+if (1 <= @ARGV){
+  $data_type = lc($ARGV[0]);
+}
+
 print<<HEADER;
 - Copyright (C) 2020 The go-sqltest Authors. All rights reserved.
 -
@@ -27,6 +32,9 @@ my $tbl_name_prefix = "test";
 my @data_types = ("INT", "FLOAT", "DOUBLE");
 my @data_values = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
+my $cnt = @data_values;
+my $min = $data_values[0];
+my $max = $data_values[$cnt - 1];
 my $sum = 0;
 my $avg = 0;
 for my $value (@data_values) {
@@ -34,31 +42,35 @@ for my $value (@data_values) {
 }
 $avg = $sum / @data_values;
 
-for (my $t = 0; $t < @data_types; $t++){
-  my $data_type = $data_types[$t];
-  my $tbl_name = $tbl_name_prefix . "_" . lc($data_type);
+my $tbl_name = $tbl_name_prefix . "_" . lc($data_type);
 
-  my $column_type = uc($data_type);
-  my $column_name = "c" . lc($data_type);
-  print "CREATE TABLE ${tbl_name} (\n";  
-  print "\t$column_name $column_type PRIMARY KEY";  
-  print "\n);\n";  
+my $column_type = uc($data_type);
+my $column_name = "c" . lc($data_type);
+print "CREATE TABLE ${tbl_name} (\n";  
+print "\t$column_name $column_type PRIMARY KEY";  
+print "\n);\n";  
+print "{\n";  
+print "}\n";
+
+for (my $n = 0; $n < @data_values; $n++){
+  my $data_value = $data_values[$n];
+  print "INSERT INTO ${tbl_name} ($column_name) VALUES ($data_value);\n";  
   print "{\n";  
   print "}\n";
+}
 
-  for (my $n = 0; $n < @data_values; $n++){
-    my $data_value = $data_values[$n];
-    print "INSERT INTO ${tbl_name} ($column_name) VALUES ($data_value);\n";  
-    print "{\n";  
-    print "}\n";
-  }
+my @sels = ("COUNT(*)", "AVG($column_name)", "SUM($column_name)", "MIN($column_name)", "MAX($column_name)");
+my @expecteds = ($cnt, $avg, $sum, $min, $max);
 
-  print "SELECT COUNT(*) FROM ${tbl_name};\n";  
+for (my $n = 0; $n < @sels; $n++){
+  my $sel = $sels[$n];
+  my $exp = $expecteds[$n];
+  print "SELECT $sel FROM ${tbl_name};\n";  
   print "{\n";  
   print "\t\"rows\" :\n";  
   print "\t[\n";  
   print "\t\t{\n";  
-  print "\t\t\t\"$column_name\" : @data_values\n";
+  print "\t\t\t\"$sel\" : $exp\n";
   print "\t\t}\n";  
   print "\t]\n";  
   print "}\n";  
