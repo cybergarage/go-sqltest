5bdd6397c8e4ed6bbaddc55a5b69fd5b7870f415
Satoshi Konno <skonno@cybergarage.org>
2023-09-14 01:01:26 +0900

    Update sqltest

--- cmd/sqltest/main.go
+++ cmd/sqltest/main.go
@@ -12,7 +12,60 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// nolint: forbidigo
 package main
 
+import (
+	"flag"
+	"os"
+
+	"github.com/cybergarage/go-logger/log"
+	"github.com/cybergarage/go-sqltest/sqltest"
+)
+
+func printUsage() {
+	println("Usage: sqltest [options] <scenario path>")
+	println("Options:")
+	println("  -host <host>")
+	println("  -port <port>")
+	println("  -protocol <protocol>")
+}
+
 func main() {
+	log.SetStdoutDebugEnbled(true)
+
+	var (
+		host     = flag.String("host", "localhost", "Host")
+		port     = flag.Int("port", 0, "Port")
+		protocol = flag.String("protocol", "tcp", "<mysql|pg>")
+	)
+	flag.Parse()
+
+	args := os.Args
+	if len(args) < 2 {
+		printUsage()
+		return
+	}
+
+	var client sqltest.Client
+	switch *protocol {
+	case "mysql":
+		client = sqltest.NewMySQLClient()
+		if *port == 0 {
+			*port = 3306
+		}
+	case "pg":
+		client = sqltest.NewPostgresClient()
+		if *port == 0 {
+			*port = 5432
+		}
+	default:
+		printUsage()
+		return
+	}
+
+	client.SetHost(*host)
+	client.SetPort(*port)
+
+	// scenarioPath := args[1]
 }
