f694a3dad8f37571bdc41512b9c5767de4dc3b75
Satoshi Konno <skonno@cybergarage.org>
2025-05-09 14:30:10 +0900

    Add Query

---
+++ sqltest/query.go
@@ -0,0 +1,38 @@
+// Copyright (C) 2020 The go-sqltest Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqltest
+
+type Query struct {
+	query string
+	args  []any
+}
+
+// NewQuery creates a new Query instance with the given SQL query and arguments.
+func NewQuery(query string, args ...any) *Query {
+	return &Query{
+		query: query,
+		args:  args,
+	}
+}
+
+// String returns the SQL query string.
+func (q *Query) String() string {
+	return q.query
+}
+
+// Aarguments returns the arguments for the SQL query.
+func (q *Query) Arguments() []any {
+	return q.args
+}
